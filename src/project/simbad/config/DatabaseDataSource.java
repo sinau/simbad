9c2ffc23d34d09f82b135c774160715b3334cc0c
Santoso <herusantoso23@github.com>
2016-04-09 19:53:56 +0700

    Model Registrasi

--- src/project/simbad/config/DatabaseDataSource.java
+++
@@ -1,5 +0,0 @@
-package project.simbad.config;
-
-public class DatabaseDataSource {
-	//TODO
-}
---
+++ src/project/simbad/model/Registrasi.java
@@ -0,0 +1,55 @@
+package project.simbad.model;
+
+public class Registrasi {
+	private String idRegistrasi;
+	private String nama;
+	private String alamat;
+	private String asalSekolah;
+	private String noTelepon;
+	private String tanggalPembuatan;
+	private String tanggalUpdate;
+	
+	public String getIdRegistrasi() {
+		return idRegistrasi;
+	}
+	public void setIdRegistrasi(String idRegistrasi) {
+		this.idRegistrasi = idRegistrasi;
+	}
+	public String getNama() {
+		return nama;
+	}
+	public void setNama(String nama) {
+		this.nama = nama;
+	}
+	public String getAlamat() {
+		return alamat;
+	}
+	public void setAlamat(String alamat) {
+		this.alamat = alamat;
+	}
+	public String getAsalSekolah() {
+		return asalSekolah;
+	}
+	public void setAsalSekolah(String asalSekolah) {
+		this.asalSekolah = asalSekolah;
+	}
+	public String getNoTelepon() {
+		return noTelepon;
+	}
+	public void setNoTelepon(String noTelepon) {
+		this.noTelepon = noTelepon;
+	}
+	public String getTanggalPembuatan() {
+		return tanggalPembuatan;
+	}
+	public void setTanggalPembuatan(String tanggalPembuatan) {
+		this.tanggalPembuatan = tanggalPembuatan;
+	}
+	public String getTanggalUpdate() {
+		return tanggalUpdate;
+	}
+	public void setTanggalUpdate(String tanggalUpdate) {
+		this.tanggalUpdate = tanggalUpdate;
+	}
+
+}
